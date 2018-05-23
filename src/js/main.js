9b145114da2c01c20f09c258b10a5fb2bf410d2f
Vladyslav Liashenko <vladyslav.liashenko@gmail.com>
2018-05-23 11:05:03 +0300

    news, single news pages, small improvements

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var giftSlider = new Swiper ('.gift-slider', {
   // Optional parameters
   direction: 'horizontal',
@@ -72,3 +74,108 @@ toggleClass('sidenav-main-trigger', 'sidenav-main', 'is-active');
 toggleClass('sidenav-main-trigger-close', 'sidenav-main', 'is-active');
 
 MicroModal.init();
+
+var Shuffle = window.Shuffle;
+
+var rewardsGrid = function (element) {
+  this.types = Array.from(document.querySelectorAll('.filterBtn'));
+
+  this.shuffle = new Shuffle(element, {
+    easing: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)', // easeOutQuart
+    sizer: '.sizer',
+  });
+
+  this.filters = {
+    types: []
+  };
+
+  this._bindEventListeners();
+};
+
+/**
+ * Bind event listeners for when the filters change.
+ */
+rewardsGrid.prototype._bindEventListeners = function () {
+  this._onTypeChange = this._handleTypeChange.bind(this);
+
+  this.types.forEach(function (button) {
+    button.addEventListener('click', this._onTypeChange);
+  }, this);
+};
+
+/**
+ * Get the values of each `active` button.
+ * @return {Array.<string>}
+ */
+rewardsGrid.prototype._getCurrentTypeFilters = function () {
+  return this.types.filter(function (button) {
+    return button.classList.contains('active');
+  }).map(function (button) {
+    return button.getAttribute('data-group');
+  });
+};
+
+/**
+ * A type button was clicked. Update filters and display.
+ * @param {Event} evt Click event object.
+ */
+rewardsGrid.prototype._handleTypeChange = function (evt) {
+  var button = evt.currentTarget;
+
+  // Treat these buttons like radio buttons where only 1 can be selected.
+  if (button.classList.contains('active')) {
+    button.classList.remove('active');
+  } else {
+    this.types.forEach(function (btn) {
+      btn.classList.remove('active');
+    });
+
+    button.classList.add('active');
+  }
+
+  this.filters.types = this._getCurrentTypeFilters();
+  this.filter();
+};
+
+/**
+ * Filter shuffle based on the current state of filters.
+ */
+rewardsGrid.prototype.filter = function () {
+  if (this.hasActiveFilters()) {
+    this.shuffle.filter(this.itemPassesFilters.bind(this));
+  } else {
+    this.shuffle.filter(Shuffle.ALL_ITEMS);
+  }
+};
+
+/**
+ * If any of the arrays in the `filters` property have a length of more than zero,
+ * that means there is an active filter.
+ * @return {boolean}
+ */
+rewardsGrid.prototype.hasActiveFilters = function () {
+  return Object.keys(this.filters).some(function (key) {
+    return this.filters[key].length > 0;
+  }, this);
+};
+
+/**
+ * Determine whether an element passes the current filters.
+ * @param {Element} element Element to test.
+ * @return {boolean} Whether it satisfies all current filters.
+ */
+rewardsGrid.prototype.itemPassesFilters = function (element) {
+  var types = this.filters.types;
+  var type = element.getAttribute('data-groups');
+
+  // If there are active type filters and this type is not in that array.
+  if (types.length > 0 && !types.includes(type)) {
+    return false;
+  }
+
+  return true;
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  window.grid = new rewardsGrid(document.querySelector('.my-shuffle-container'));
+});
